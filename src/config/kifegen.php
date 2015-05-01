c966cacdad91624e29d5525070ca49e8f497f011
Vincent Reynaert <vincent@kifed.be>
2015-05-01 12:45:29 +0200

    improve config

--- src/config/kifegen.php
+++ src/config/kifegen.php
@@ -1,7 +1,17 @@
 <?php
 
 return [
-    'model_path' => './app/Models',
-    'contract_path' => './app/Repositories/',
-    'implementation_path' => './app/Repositories/Eloquent/'
+
+	// Model settings
+	'model_name'      => '{{name}}Model',
+	'model_path'      => 'app/',
+
+	// Repository settings
+	'contract_name'   => '{{name}}Reeepository',
+	'contract_path'   => 'app/Repositories/',
+
+	// Repository settings
+	'repository_name' => 'Eloquent{{name}}Repository',
+	'repository_path' => 'app/Repositories/Eloquent/',
+
 ];
